1e7518088e39f6c40aa2c29e87212480e3120ac3
Subhan-khaliq <subhankhaiq420@gmail.com>
2022-09-07 11:44:35 +0500

    Changes

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,3 @@
 //= link_tree ../images
-//= link_directory ..packs/stylesheets .css
+//= link_directory ../stylesheets .css
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
-// import 'bootstrap'
+import 'bootstrap'
 import '../stylesheets/application'
 Rails.start()
 Turbolinks.start()
